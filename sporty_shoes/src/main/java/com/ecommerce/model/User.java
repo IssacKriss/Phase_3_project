878782dde8eaf7ebd982858e02a49d367afae205
IssacKriss <50828949+IssacKriss@users.noreply.github.com>
2020-08-20 21:12:21 +0530

    this is my first commit till user class

---
+++ sporty_shoes/src/main/java/com/ecommerce/model/User.java
@@ -0,0 +1,48 @@
+package com.ecommerce.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+
+@Entity
+@Table(name= "users") 
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int user_id;
+	
+//    @Column(nullable = false)
+    private String user_name;
+    
+//    @Column(nullable = false)
+    private int age;
+    
+//    @Column(nullable = false)
+    private String address;
+    
+//    @Column(nullable = false)
+    private Date date_added;
+    
+//    @Column(nullable = false)
+	private String email_id;
+    
+//    @Column(nullable = false)
+	private String password;
+}
